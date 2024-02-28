8f0f696e0b9f4669b4d70596a599341cafa9b60a
Somz <somz@Somzs-MacBook-Air.local>
2024-02-28 21:47:12 +0800

    added Class6_assignment2

---
+++ Class6_Assigment2.java
@@ -0,0 +1,74 @@
+package Assignments;
+
+public class Class6_Assigment2 {
+	
+	//create a main method class and create 5 extra static methods each for add,sub,mult,div,modules
+
+	
+
+		public static void main(String[] args) 
+
+		{
+
+			add(); 	sub(); mul(); div(); mod();
+
+		}
+
+		 static void add()
+
+		{
+
+			 int num1=2; byte num2 =100;
+
+			System.out.println("Addition = "+(num1+num2));
+
+		}
+
+
+		static void sub()
+
+		
+
+		{
+
+			int num1=2; byte num2 =100;
+
+			System.out.println("Subtraction = "+(num2-num1));
+
+		}
+
+		static void mul()
+
+		
+
+		{
+
+			int num1=2; byte num2 =100; 
+
+			System.out.println("Multiplication = "+(num1*num2));
+
+		}
+
+		static void div()
+
+		{
+
+			int num1=2; byte num2 =100; 
+
+			System.out.println("Division = "+(num2/num1));
+
+		}
+
+		static void mod()
+
+		{
+
+			int num1=2; double num3=2.05;
+
+			System.out.println("Modulus = "+(num3%num1));	
+
+		}
+
+		
+
+}
